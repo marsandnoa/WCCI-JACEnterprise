c2c519c598d1570b98ca1152be69ab22a41df2d5
unknown <marsandnoa@gmail.com>
2023-09-01 20:28:41 -0400

    making backend

---
+++ api/src/main/java/com/jec/api/controllers/ApiRequestController.java
@@ -0,0 +1,108 @@
+package com.jec.api.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jec.api.entities.BoredArticle;
+import com.jec.api.entities.NasaArticle;
+import com.jec.api.services.NasaService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiRequestController {
+
+    @Autowired
+    NasaService articleServ;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping("/bored")
+    public List<BoredArticle> getBored() {
+        List<BoredArticle> output = new ArrayList<>();
+        while (output.size() < 10) {
+            HttpHeaders headers = new HttpHeaders();
+            String apiUrl = "http://www.boredapi.com/api/activity/";
+            ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET,
+                    new HttpEntity<>(headers),
+                    String.class);
+            String response = responseEntity.getBody();
+            JsonNode jsonElement;
+            try {
+                jsonElement = objectMapper.readTree(response);
+                BoredArticle currentArticle = new BoredArticle();
+                currentArticle.setActivity(jsonElement.get("activity").asText());
+                currentArticle.setAccessibility(jsonElement.get("accessibility").asText());
+                currentArticle.setLink(jsonElement.get("link").asText());
+                currentArticle.setParticipants(jsonElement.get("participants").asText());
+                output.add(currentArticle);
+            } catch (JsonMappingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JsonProcessingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return output;
+    }
+
+    @GetMapping("/NASA")
+    public List<NasaArticle> getNASA() {
+        List<NasaArticle> output = new ArrayList<>();
+
+        HttpHeaders headers = new HttpHeaders();
+        String apiKey = "9Ywd2QQ6znC1eOkRYRb3X0trteKgNO5D7LfKdejD";
+        headers.set("api_key", apiKey);
+
+        String apiUrl = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey
+                + "&start_date=2023-08-01&end_date=2023-08-15";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, new HttpEntity<>(headers),
+                String.class);
+        String response = responseEntity.getBody();
+
+        try {
+            JsonNode jsonArray = objectMapper.readTree(response);
+            if (jsonArray.isArray()) {
+                for (JsonNode jsonElement : jsonArray) {
+                    String title = jsonElement.get("title").asText();
+                    String explanation = jsonElement.get("explanation").asText();
+                    String url = jsonElement.get("url").asText();
+                    String author;
+                    try {
+                        author = jsonElement.get("copyright").asText();
+                    } catch (Exception e) {
+                        author = "PUBLIC IMAGE";
+                    }
+
+                    NasaArticle currentArticle = new NasaArticle();
+                    author = author.replace("\n", " ");
+                    currentArticle.setAuthor(author);
+                    currentArticle.setImage(url);
+                    currentArticle.setTitle(title);
+                    currentArticle.setSummary(explanation);
+
+                    output.add(currentArticle);
+                }
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+}
