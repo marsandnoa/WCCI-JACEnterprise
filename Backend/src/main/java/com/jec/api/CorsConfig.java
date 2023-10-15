f68713a79e8f53c9b3fa8294a3111b37e3d5a05a
unknown <marsandnoa@gmail.com>
2023-10-15 16:01:36 -0400

    working compose

--- Backend/src/main/java/com/jec/api/CorsConfig.java
+++ Backend/src/main/java/com/jec/api/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
